2539b907ff01ba70b4f9f9051cadce2b37785129
Unai <unai_des@hotmail.com>
2022-06-07 10:28:11 +0200

    Math(JavaScript)

---
+++ Frontend/JavaScript/002_Métodos de objeto/004_Math/script.js
@@ -0,0 +1,38 @@
+//Math
+//Propiedades
+//Pi E LN2 LN10, SQRT1_2 SQRT2
+console.log(Math.PI);
+
+//Métodos
+
+//Números random entre 1-10
+console.log(parseInt(Math.random() * 10 + 1));
+
+//Max y Min
+console.log(Math.max(2, 3, 1, 5, 10));
+console.log(Math.min(2, 3, 1, 5, 10));
+
+//Redondear
+console.log(Math.round(5.78));
+//Redondear al alza
+console.log(Math.ceil(5.78));
+//Redondear a la baja
+console.log(Math.floor(5.78));
+
+//Potencia
+console.log(Math.pow(2, 2));
+
+//Valor absoluto
+console.log(Math.abs(-4));
+
+//Devolver la parte emtera
+console.log(Math.trunc(13.745));
+
+//Devolver el signo del número
+//1y0(positivo) -1y-0(negativo)
+console.log("-----Signo----");
+
+console.log(Math.sign(-8));
+console.log(Math.sign(0));
+console.log(Math.sign(NaN));
+console.log(Math.sign(45));
